1bfb27471b1cdf6e714b032bef46b6b8142014a9
Algure <ajirie98@gmail.com>
2022-03-28 04:27:46 +0100

    Best practice cleanup

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from flask import render_template
 app = Flask(__name__, )
 # app = Flask(__name__, template_folder='templates')
 
-FLUTTER_WEB_APP = 'templates'
 
 
 @app.route('/')
 def render_page():
     return render_template('/index.html')
 
+FLUTTER_WEB_APP = 'templates'
 
 @app.route('/web/')
 def render_page_web():
@@ -21,7 +21,7 @@ def render_page_web():
 
 
 @app.route('/web/<path:name>')
-def flutter_redirect(name):
+def return_flutter_doc(name):
 
     dData = str(name).split('/')
     DIR_NAME = FLUTTER_WEB_APP
@@ -30,7 +30,7 @@ def flutter_redirect(name):
         for i in range(0, len(dData) - 1):
             DIR_NAME += '/' + dData[i]
 
-    if ('#' in dData[-1] or dData[-1] == '#/' or dData[-1] == '#'):
+    if ('#' in dData[-1]):
         dData[-1] = 'index.html'
 
     return send_from_directory(DIR_NAME, dData[-1])
